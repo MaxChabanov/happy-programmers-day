5c97f69e5cffee47c1a1016ac10c82500b43b31e
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-09-13 15:16:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function fireConfetti() {
+  var count = 300;
+  var defaults = {
+    origin: { y: -0.4 },
+    angle: 270,
+  };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 60,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 80,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  fire(0.1, {
+    spread: 140,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  fire(0.1, {
+    spread: 140,
+    startVelocity: 45,
+  });
+}
+
+fireConfetti();
+
+let emojiElements = document.querySelectorAll(".emoji");
+
+emojiElements.forEach((el) => {
+  el.addEventListener("click", fireConfetti);
+});
